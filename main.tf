a2705fe549c0d0c78a71f166112249c5e2b57196
haiduongacm <63597461+haiduongacm@users.noreply.github.com>
2023-02-10 15:08:54 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,14 +2,6 @@ provider "aws" {
   region = "ap-southeast-1"
 }
 
-#module "vpce" {
-#  source = "./vpce"
-#vpc_id = "vpc-008ff47fce4b220b6"
-#subnet_ids =["subnet-0818c62fce0c6e142"]
-#security_group_ids = ["sg-082585bbffaea34c0"]
-#}
-
-
 resource "aws_vpc_endpoint" "ecr-api" {
   vpc_id       = var.vpc_id
   service_name = "com.amazonaws.ap-southeast-1.ecr.api"
