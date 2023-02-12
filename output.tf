76f03c3d04e8d3942cefe2bb6cdf88cde32edfb0
haiduongacm <63597461+haiduongacm@users.noreply.github.com>
2023-02-12 16:08:15 +0700

    Delete output.tf

--- output.tf
+++
@@ -1,9 +0,0 @@
-output "vpce-api_id" {
-  description = "get id vpce to serivce ecr api"
-  value = aws_vpc_endpoint.ecr-api.id
-}
-
-output "vpce-dkr_id" {
-  description = "get id vpce to serivce ecr api"
-  value = aws_vpc_endpoint.ecr-dkr.id
-}
